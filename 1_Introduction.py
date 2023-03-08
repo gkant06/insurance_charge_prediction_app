8264dd310323aef5e31747d35b9db03d4e9e3371
gkant06 <105669929+gkant06@users.noreply.github.com>
2023-03-08 00:42:33 -0500

    Add files via upload

---
+++ 1_Introduction.py
@@ -0,0 +1,7 @@
+# import module
+import streamlit as st
+
+# Title
+st.title("Hello!!!")
+
+st.write("# Here is an analysis on insurance charges and factors that affect it. We also have a charges calculator that will estimate your insurance charge based on your gender, age, region and other information. Check it out!")
